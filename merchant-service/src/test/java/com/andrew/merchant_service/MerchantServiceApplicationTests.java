b23831687e2fa586707af12db2d9d12034616473
Andrew Ezzat <andrewezzat21@gmail.com>
2025-04-10 13:50:01 +0200

    setup merchant service

---
+++ merchant-service/src/test/java/com/andrew/merchant_service/MerchantServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrew.merchant_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MerchantServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
